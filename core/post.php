aee81de56d67a7bb9d47d65ed5f13e67d56d24d4
BakelohNoel <bakelohnoel@gmail.com>
2024-06-03 15:07:43 +0300

    declaration

--- core/post.php
+++ core/post.php
@@ -1,80 +1,85 @@
 <?php
 
-class post(
-    //db stuff
+class Post {
+    // db stuff
     private $conn;
     private $table = "posts";
 
-    //post properties
+    // post properties
     public $id;
     public $category_id;
     public $category_name;
     public $title;
     public $body;
-    public $author_at;
+    public $author;
+    public $created_at;
 
-    //constaractor with db connection
-    public function __construct($db){
-       $this->conn = $db;
-    } 
-    //getting post from database
-    public function read(){
-        //create query
+    // constructor with db connection
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // get posts from database
+    public function read() {
+        // create query
         $query = 'SELECT 
-        c.name as category_name,
-        p.id,
-        p.category_id,
-        p.title,
-        p.body,
-        p.author,
-        p.create_at
+            c.name as category_name,
+            p.id,
+            p.category_id,
+            p.title,
+            p.body,
+            p.author,
+            p.created_at
         FROM
-        '.$this->table.'  p
+            ' . $this->table . ' p
         LEFT JOIN
-        categories c ON p.category_id = c.id
-        ORDER BY p.created_at DESC';
+            categories c ON p.category_id = c.id
+        ORDER BY
+            p.created_at DESC';
 
-        //prepare statement
+        // prepare statement
         $stmt = $this->conn->prepare($query);
-        //execute query
+        // execute query
         $stmt->execute();
         return $stmt;
     }
 
-   public function read_single()(
-    $query = 'SELECT 
-    c.name as category_name,
-    p.id,
-    p.category_id,
-    p.title,
-    p.body,
-    p.author,
-    p.create_at
-    FROM
-    '.$this -> table . '  p
-    LEFT JOIN
-    categories c ON p.category_id = c.id
-    WHERE p.id = ? LIMIT 1';
-    
-    //prepare statement
-    $stmt = $this->conn->prepare($query);
-    //bind given post id to parameter
-    $stmt->bindParam(':post_id', $this->id);
-    //execute the prepared statement
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    public function read_single() {
+        $query = 'SELECT 
+            c.name as category_name,
+            p.id,
+            p.category_id,
+            p.title,
+            p.body,
+            p.author,
+            p.created_at
+        FROM
+            ' . $this->table . ' p
+        LEFT JOIN
+            categories c ON p.category_id = c.id
+        WHERE
+            p.id = ?
+        LIMIT
+            1';
+        
+        // prepare statement
+        $stmt = $this->conn->prepare($query);
+        // bind post ID to parameter
+        $stmt->bindParam(1, $this->id);
+        // execute the prepared statement
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $this->title = $row['title'];
-    $this->category_name = $row['category_name'];
-    $this->body = $row['body'];
-    $this->author = $row['author'];
-    $this->category_id =  $row['category_id'];
-
-
-   )
-)
+        $this->title = $row['title'];
+        $this->category_name = $row['category_name'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+    }
+}
 
 ?>
 
-//continue from 
-//https://www.youtube.com/watch?v=myZ66m0C8VQ&list=PLS1QulWo1RIYWjdoEC1WbT8W3XGGWVXfW&index=6
+
+<!-- //continue from 
+//https://www.youtube.com/watch?v=myZ66m0C8VQ&list=PLS1QulWo1RIYWjdoEC1WbT8W3XGGWVXfW&index=6 -->
